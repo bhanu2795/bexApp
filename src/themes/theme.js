84eca8ecf1c8352eadecd7962b6421ca519e6234
bhanu singh <bhanu.singh@bblab.biz>
2019-05-16 15:46:18 +0530

    Basic theme config

---
+++ src/themes/theme.js
@@ -0,0 +1,54 @@
+const THEME_CONFIG = {
+    fontProp: {
+        xlFont: 21,
+        lgFont: '',
+        mdFont: 13,
+        smFont: '',
+        xsFont: '',
+        fontFamily: {
+            light: {
+                italic: 'Montserrat-LightItalic',
+                normal: 'Montserrat-Light'
+            },
+            medium: {
+                italic: 'Montserrat-MediumItalic',
+                normal: 'Montserrat-Medium'
+            },
+            bold: {
+                italic: 'Montserrat-ExtraBoldItalic',
+                normal: 'Montserrat-Bold'
+            },
+            regular: 'Montserrat-Regular'
+        }
+    },
+    alignProp: {
+        defaultPadding: 10,
+        defaultMargin: 10
+    },
+    color: {
+        primaryColor: {
+            accentColor: '#673ab6',
+            inactive: '#737272'
+        },
+        secondaryColor: {
+
+        },
+        tertiary: {
+
+        },
+        success: {
+
+        },
+        warning: {
+
+        },
+        danger: {
+
+        },
+        common: {
+            white: '#fff'
+        }
+    }
+};
+
+export default THEME_CONFIG;
